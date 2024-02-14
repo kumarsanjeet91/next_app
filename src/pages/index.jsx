53b53e18b2795cc6ee85198052731151b566ad3a
sanjeet_kumar <141094303+sanjeetucet@users.noreply.github.com>
2024-02-14 23:38:34 +0530

    head and script

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,18 @@
 import Head from "next/head";
 import Script from "next/script";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Portfolio</title>
       </Head>
-      <h1>My name is Sanjeet</h1>
-      <Script src="./sc.js" strategy="lazyOnload"></Script>
+      <center>
+        <div className="mt-5">
+          <h1 className="text-3xl">My name is Sanjeet</h1>
+          <p>A blog post by sanjeet kumar</p>
+        </div>
+      </center>
     </>
   );
 }
